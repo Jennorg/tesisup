523d81858399add554f9e74a0863ea66cd53d9c0
Andres <98991190+Andres1555@users.noreply.github.com>
2025-06-20 02:15:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@/components/Home";
 import Login from "@/components/Auth/Login";
 import SignUp from "@/components/Auth/Signup";
-import MainPage from "@/components/main/MainPage"
-import Perfil from "./components/Perfil/Perfil";
+import MainPage from "@/components/main/MainPage";
+import Perfil from "@/components/Perfil/Perfil";
+import PerfilEstudiantes from "@/components/estudiantes/perfil_estudiantes";
+import EncontrarEstudiantes from "@/components/estudiantes/encontrarestudiantes";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signUp" element={<SignUp />}></Route>
-      <Route path="/mainPage" element={<MainPage />}></Route>
-      <Route path="/Perfil" element={<Perfil />}></Route>
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/mainPage" element={<MainPage />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/perfil_estudiantes" element={<PerfilEstudiantes />} />
+      <Route path="/encontrarestudiantes" element={<EncontrarEstudiantes />} />
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
-}
+};
 
 export default App;

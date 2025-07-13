7d6690797006acf7d18497bd01f2fa7fb22a0d64
Andres <98991190+Andres1555@users.noreply.github.com>
2025-07-13 14:24:19 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div className="grid place-items-center w-dvw h-dvh">
-            <h1>Home</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/signUp">Sign Up</Link>
-            <Link to="/perfil_estudiantes">Perfil Estudiantes</Link> 
+  return (
+    <div className="grid place-items-center w-dvw h-dvh bg-blue-600">
+      <div className="bg-gray-800 p-8 rounded-xl shadow-xl w-[90%] max-w-sm h-[500px] flex flex-col justify-start text-white space-y-5">
+        
+        {/* Logo UNEG */}
+        <img
+          src="/img/uneg-logo.png"
+          alt="Logo UNEG"
+          className="mx-auto w-20 h-20 mb-1"
+        />
+
+        {/* Título principal */}
+        <h1 className="text-3xl font-bold text-center mb-4 mt-1">Digitalys</h1>
+
+        {/* Botones de navegación */}
+        <div className="flex flex-col gap-5 w-full px-3 mt-2">
+          <div className="bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg transition">
+            <Link
+              to="/login"
+              className="block py-3 text-white text-lg font-medium text-center"
+            >
+              Login
+            </Link>
+          </div>
+          <div className="bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg transition">
+            <Link
+              to="/signUp"
+              className="block py-3 text-white text-lg font-medium text-center"
+            >
+              Sign Up
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;

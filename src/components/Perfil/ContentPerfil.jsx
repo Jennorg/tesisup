b2a77e8a074001468f75249edc9aea4c1647fcc6
Andres <98991190+Andres1555@users.noreply.github.com>
2025-07-13 14:19:16 -0400

    Update ContentPerfil.jsx

--- src/components/Perfil/ContentPerfil.jsx
+++ src/components/Perfil/ContentPerfil.jsx
@@ -1,17 +1,17 @@
 import { React } from 'react'
 
-const ContentPerfil = ({ isAsideVisible }) => {
+const ContentPerfil = ({ isAsideVisible, user }) => {
   const marginLeftClass = isAsideVisible ? 'ml-16' : 'ml-0';
 
   return (
     <div className={`flex-grow w-full p-4 overflow-ellipsis transition-margin-left duration-300 ease-in-out ${marginLeftClass}`}>
-      <h1>Nombre</h1>
-      <h2>Cargo</h2>
-      <h3>Correo</h3>
-      <h4>Telefono</h4>
-      <h5>Direccion</h5>
+      <h1>{user?.nombre || "Nombre no disponible"}</h1>
+      <h2>{user?.cargo || "Cargo no disponible"}</h2>
+      <h3>{user?.email || "Correo no disponible"}</h3>
+      <h4>{user?.telefono || "Teléfono no disponible"}</h4>
+      <h5>{user?.direccion || "Dirección no disponible"}</h5>
     </div>
-  )
-}
+  );
+};
 
-export default ContentPerfil
+export default ContentPerfil;
